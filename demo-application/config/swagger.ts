36cf8885a16ad0439fe0ec34488892fd204f16cd
Niklas Haug <niklas.haug@hs-augsburg.de>
2024-11-26 16:19:16 +0100

    added autoswagger package to generate openapi specification automatically from adonis project

---
+++ demo-application/config/swagger.ts
@@ -0,0 +1,29 @@
+import path from 'node:path'
+import url from 'node:url'
+
+export default {
+  path: path.dirname(url.fileURLToPath(import.meta.url)) + '/../',
+  title: 'Foo', // use info instead
+  version: '1.0.0', // use info instead
+  description: '', // use info instead
+  tagIndex: 2,
+  info: {
+    title: 'title',
+    version: '1.0.0',
+    description: '',
+  },
+  snakeCase: true,
+
+  debug: false, // set to true, to get some useful debug output
+  ignore: ['/swagger', '/docs'],
+  preferredPutPatch: 'PUT', // if PUT/PATCH are provided for the same route, prefer PUT
+  common: {
+    parameters: {}, // OpenAPI conform parameters that are commonly used
+    headers: {}, // OpenAPI conform headers that are commonly used
+  },
+  securitySchemes: {}, // optional
+  authMiddlewares: ['auth', 'auth:api'], // optional
+  defaultSecurityScheme: 'BearerAuth', // optional
+  persistAuthorization: true, // persist authorization between reloads on the swagger page
+  showFullPath: false, // the path displayed after endpoint summary
+}
