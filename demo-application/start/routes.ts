d07ca03802c0f483a66fc284c927e935c27456af
Niklas Haug <niklas.haug@hs-augsburg.de>
2024-11-26 14:49:30 +0100

    added admin routes with missing check for admin role

--- demo-application/start/routes.ts
+++ demo-application/start/routes.ts
@@ -8,9 +8,27 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import User from '#models/user'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router
+  .group(() => {
+    /**
+     * All routes registered inside the callback
+     * should only be accessible by admin users
+     * This check is currently missing on purpose
+     */
+    router.get('/users', async () => {
+      return User.query()
+    })
+
+    router.get('/users/:id', async ({ params }) => {
+      return User.findOrFail(params.id)
+    })
+  })
+  .prefix('/admin')
