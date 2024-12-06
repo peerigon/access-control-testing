f777761d88f3cdf19fbde0868007d486ba1dad55
Niklas Haug <niklas.haug@hs-augsburg.de>
2024-12-06 14:50:11 +0100

    consume access-control-testing package locally for testing purposes

---
+++ demo-application/tests/accessControlTesting.ts
@@ -0,0 +1,6 @@
+import { helloWorld } from 'access-control-testing'
+
+helloWorld()
+
+// todo: seed the database with some fake data as before hook
+// todo: provide necessary data to the tool and start the scan
