a86a0742aef7e80ea9984ee102beae0775964eb8
Niklas Haug <niklas.haug@peerigon.com>
2024-12-16 17:10:04 +0100

    enhanced constants with openapi field names

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,2 +1,16 @@
-export const MODULE_NAME = "act";
-export const OPENAPI_RESOURCE_NAME_FIELD = "x-act-resource-name";
+export const TOOL_NAME = "act";
+
+const OPENAPI_FIELD_PREFIX = `x-${TOOL_NAME}-`;
+const OpenApiFieldNames: Record<string, string> = {
+  RESOURCE_NAME: "resource-name",
+  AUTH_ENDPOINT: "auth-endpoint",
+};
+export const OpenApiFields = new Proxy(OpenApiFieldNames, {
+  get(target, prop) {
+    if (prop in target && typeof prop === "string") {
+      const fieldName = target[prop];
+      return OPENAPI_FIELD_PREFIX + fieldName;
+    }
+    return null;
+  },
+});
