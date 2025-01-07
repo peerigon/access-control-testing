69d926157851e78a0db35b9047b6add85f9c655e
Niklas Haug <niklas.haug@peerigon.com>
2025-01-07 19:36:54 +0100

    added login capability with token to demo-application

--- demo-application/start/routes.ts
+++ demo-application/start/routes.ts
@@ -7,8 +7,10 @@
 |
 */
 
+// import hash from '@adonisjs/core/services/hash'
 import router from '@adonisjs/core/services/router'
 import User from '#models/user'
+import { middleware } from '#start/kernel'
 
 router.get('/', async () => {
   return {
@@ -31,4 +33,26 @@ router
       return User.findOrFail(params.id)
     })
   })
+  .use(middleware.auth())
   .prefix('/admin')
+
+router.post('/login', async ({ request }) => {
+  const { email, password } = request.body()
+
+  //const h = await hash.make(password)
+
+  const user = await User.verifyCredentials(email, password)
+
+  const token = await User.accessTokens.create(
+    user, // for user
+    ['*'], // with all abilities
+    {
+      expiresIn: '30 days', // expires in 30 days
+    }
+  )
+
+  console.log('Login successful ')
+  return {
+    token,
+  }
+})
