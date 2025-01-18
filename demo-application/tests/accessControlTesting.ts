008442878359f41a4c675daca457b386afc49173
Niklas Haug <niklas.haug@peerigon.com>
2025-01-18 14:41:43 +0100

    call Act constructor with param

--- demo-application/tests/accessControlTesting.ts
+++ demo-application/tests/accessControlTesting.ts
@@ -1,13 +1,34 @@
 // todo: replace this with from 'access-control-testing' later
-import { Act } from '../../src/api'
+import { Act, User } from '../../src/api'
 
 const test = async () => {
   // todo: seed the database with testing data
 
-  //const users = [new User('admin', 'admin'), new User('user', 'password')]
+  const user1 = new User('user1', 'password')
+  const user2 = new User('user2', 'password')
+  const adminUser1 = new User('admin', 'admin')
+
+  // second parameter is the authorization matrix, maps access types to required privileges
+  /*  const todoResource = new Resource('Todo', {
+    create: Privilege.CREATOR || Privilege.OWNER,
+    read: Privilege.VIEWER || Privilege.OWNER,
+    update: Privilege.EDITOR || Privilege.OWNER,
+    delete: Privilege.OWNER, // OWNER is default for each access type
+  })*/
+
+  // pseudocode for defining relationships
+  // openapi definition has defined for each route: type of ressource & type of access (create/read/update/delete)
+  /* user1.owns(todoResource, 123)
+  user2.canView(todoResource, 123)*/
+
+  // relationships define the privileges a specific user has for a specific resource
+
+  /*const users = [user1, adminUser1]
+  const resources = [todoResource]*/
 
   console.log('======')
-  const act = new Act()
+  // that way, tool users can use .env variables which would not be possible with the config file
+  const act = new Act('http://localhost:3333')
   await act.scan()
 }
 
