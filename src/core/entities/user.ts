d3eb17be13b9dc0d2da8ec66ffc5e493fe66211f
Niklas Haug <niklas.haug@peerigon.com>
2025-01-18 20:27:14 +0100

    draft for User resource mapping inside of User class

--- src/core/entities/user.ts
+++ src/core/entities/user.ts
@@ -1,4 +1,9 @@
+import { Resource } from "./resource";
+
 export class User {
+  // todo: this needs to include specific resource metadata to identify a specific resource
+  private readonly _relatedResources: Map<Resource, RelationshipType> =
+    new Map();
   constructor(
     private readonly _identifier: string,
     private readonly _password: string,
@@ -13,5 +18,27 @@ export class User {
   }
 
   // methode relateTo(resource: Resource, relationshipType: RelationshipType): void {
-  // methode owns(resource: Resource): return relateTo(resource, RelationshipType.OWNERSHIP);
+  public relateTo(resource: Resource, relationshipType: RelationshipType) {
+    // todo: clarify what should happen when a resource is related multiple times
+    // combine all the relationshipTypes to a privilege map
+    // or throw an error in this case?
+
+    this._relatedResources.set(resource, relationshipType);
+  }
+
+  public owns(resource: Resource) {
+    this.relateTo(resource, RelationshipType.OWNERSHIP);
+  }
+
+  public canView(resource: Resource) {
+    this.relateTo(resource, RelationshipType.VIEWER);
+  }
+
+  public canEdit(resource: Resource) {
+    this.relateTo(resource, RelationshipType.EDITOR);
+  }
+
+  public canCreate(resource: Resource) {
+    this.relateTo(resource, RelationshipType.CREATOR);
+  }
 }
