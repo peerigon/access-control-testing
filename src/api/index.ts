9c8384df0d9b560eabd234ceeef964327bbc4985
Niklas Haug <niklas.haug@peerigon.com>
2025-01-18 14:39:41 +0100

    added baseUrl to Act constructor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,24 @@
 import { apiClient } from "@japa/api-client";
 import { expect } from "@japa/expect";
 import { configure, run } from "@japa/runner";
+import { Resource } from "../core/entities/resource";
+import { User } from "../core/entities/user";
 
-export class Act {
+class Act {
+  get baseUrl(): URL {
+    return this._baseUrl;
+  }
+  private readonly _baseUrl: URL;
+
+  // todo: maybe move all the configuration stuff from the file to the constructor?
+  constructor(baseUrlString: string) {
+    try {
+      this._baseUrl = new URL(baseUrlString);
+    } catch (error) {
+      throw new Error("Invalid base URL");
+    }
+  }
+
   private async runTests() {
     // todo: set manually, not via cli args
     // processCLIArgs(process.argv.splice(2));
@@ -16,7 +32,7 @@ export class Act {
       ],
       // todo: make base url configurable as param
       // read the value from a config prop or the OpenAPI spec
-      plugins: [expect(), apiClient("http://localhost:3333")],
+      plugins: [expect(), apiClient(this._baseUrl.toString())],
     });
 
     void run();
@@ -28,3 +44,5 @@ export class Act {
     await this.runTests();
   }
 }
+
+export { Act, User, Resource };
