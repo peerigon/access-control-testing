358dd9cbaf07253f30be04132c49b7a62a2321ef
Niklas Haug <niklas.haug@peerigon.com>
2025-01-24 18:28:53 +0100

    first unit test case (#19)

---
+++ bin/test.ts
@@ -0,0 +1,19 @@
+import { apiClient } from "@japa/api-client";
+import { expect } from "@japa/expect";
+import { configure, processCLIArgs, run } from "@japa/runner";
+
+// todo: remove this file
+processCLIArgs(process.argv.splice(2));
+configure({
+  suites: [
+    {
+      name: "unit",
+      files: ["tests/unit/**/*.spec.ts"],
+    },
+  ],
+  // todo: make base url configurable as param
+  // read the value from a config prop or the OpenAPI spec
+  plugins: [expect(), apiClient("http://localhost:3333")],
+});
+
+void run();
