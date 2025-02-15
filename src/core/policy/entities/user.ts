386bee3a50c316a6449f0524302d174b5ba99e2a
Niklas Haug <niklas.haug@peerigon.com>
2025-02-15 23:44:12 +0100

    make sure userId is unique

--- src/core/policy/entities/user.ts
+++ src/core/policy/entities/user.ts
@@ -2,10 +2,22 @@ import { AuthenticationCredentials } from "../../authentication/http/types.ts";
 import { RelationshipManager } from "../relationship-manager.ts";
 
 export class User extends RelationshipManager {
+  private static readonly userIdentifiers: Set<string> = new Set();
   constructor(
     private readonly identifier: string,
     private readonly password: string,
   ) {
+    const identifierAlreadyAdded = User.userIdentifiers.has(identifier);
+
+    // todo: unit test
+    if (identifierAlreadyAdded) {
+      throw new Error(
+        `User with identifier '${identifier}' can't be defined twice as it has already been defined before.`,
+      );
+    }
+
+    User.userIdentifiers.add(identifier);
+
     super();
   }
 
