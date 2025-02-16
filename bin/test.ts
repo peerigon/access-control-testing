809f1292f2d094d826aa86bd957f9215626e9c8a
Niklas Haug <niklas.haug@peerigon.com>
2025-02-16 01:26:21 +0100

    removed apiClient for unit testing

--- bin/test.ts
+++ bin/test.ts
@@ -1,5 +1,4 @@
 // todo: decide on using node test runner or japa test runner
-import { apiClient } from "@japa/api-client";
 import { expect } from "@japa/expect";
 import { configure, processCLIArgs, run } from "@japa/runner";
 
@@ -11,9 +10,7 @@ configure({
       files: ["tests/unit/**/*.spec.ts"],
     },
   ],
-  // todo: make base url configurable as param
-  // read the value from a config prop or the OpenAPI spec
-  plugins: [expect(), apiClient("http://localhost:3333")],
+  plugins: [expect()],
 });
 
 void run();
