7d4de4a5d4b9cb9e3d3f584e3c69cb3051de372a
Niklas Haug <niklas.haug@peerigon.com>
2025-02-28 17:58:52 +0100

    replaced deprecated .eslintignore file with ignores in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 // We have a couple of presets for popular setups, such as typescript-react or typescript-node
 // See "Presets"
 import typescriptNodePreset from "@peerigon/configs/eslint/presets/typescript-node";
-// https://github.com/peerigon/configs/blob/main/eslint/README.md
 
-export default typescriptNodePreset;
+// https://github.com/peerigon/configs/blob/main/eslint/README.md
+export default [
+    ...typescriptNodePreset.map((config) => ({
+        ...config,
+        ignores: [...(config.ignores ?? []), "demo-application/**/*"],
+    })),
+];
